3c43bfab66f456b6d22cc06f14af398af01df312
Tvisha30 <tvishakohli3@gmail.com>
2026-07-11 13:58:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let express = require('express')
+let app = express();
+let cors = require('cors');
+
+let port = 9000;
+app.use(cors());
+app.get('/',(req,res)=>{
+    res.send({
+        message: 'Hello World!'
+    });
+});
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+});
